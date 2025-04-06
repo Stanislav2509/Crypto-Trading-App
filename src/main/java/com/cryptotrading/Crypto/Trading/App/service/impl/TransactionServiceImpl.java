117b8218df7b7f1c0e67264c6328246220039ec0
Stanislav2509 <staso03@abv.bg>
2025-04-06 21:38:29 +0300

    Transaction history

--- src/main/java/com/cryptotrading/Crypto/Trading/App/service/impl/TransactionServiceImpl.java
+++ src/main/java/com/cryptotrading/Crypto/Trading/App/service/impl/TransactionServiceImpl.java
@@ -1,23 +1,34 @@
 package com.cryptotrading.Crypto.Trading.App.service.impl;
 
 import com.cryptotrading.Crypto.Trading.App.model.entity.Transaction;
+import com.cryptotrading.Crypto.Trading.App.model.entity.User;
 import com.cryptotrading.Crypto.Trading.App.repo.TransactionRepository;
+import com.cryptotrading.Crypto.Trading.App.repo.UserRepository;
 import com.cryptotrading.Crypto.Trading.App.service.TransactionService;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
 public class TransactionServiceImpl implements TransactionService {
     private final TransactionRepository transactionRepository;
+    private final UserRepository userRepository;
 
-    public TransactionServiceImpl(TransactionRepository transactionRepository) {
+    public TransactionServiceImpl(TransactionRepository transactionRepository, UserRepository userRepository) {
         this.transactionRepository = transactionRepository;
+        this.userRepository = userRepository;
     }
 
     @Override
     public Transaction findById(Long id) {
         Optional<Transaction> byId = transactionRepository.findById(id);
         return byId.orElse(null);
+    }
+
+    @Override
+    public List<Transaction> getHistoryByEmail(String name) {
+        Optional<User> userOpt = userRepository.findByEmail(name);
+        return userOpt.map(transactionRepository::findAllByUser).orElse(null);
     }
 }
